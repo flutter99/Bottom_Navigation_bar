fe1ca560d61f0dc3ac552575d750c4bfc386294a
Bilal Ahmad <thinkingall78@gmail.com>
2021-06-18 19:27:46 +0500

    Notch margin button add
    Bottom navigation complete

--- bottom_navigation_bar_1/lib/BottomAppBar.dart
+++ bottom_navigation_bar_1/lib/BottomAppBar.dart
@@ -12,6 +12,7 @@ class Bottombar extends StatefulWidget {
 
 class _BottombarState extends State<Bottombar> {
 
+   // List<Map<String, Object>> _screens;
   int currentIndex = 0;
 
   List _screens = [
@@ -22,40 +23,101 @@ class _BottombarState extends State<Bottombar> {
     ProfilePage(),
   ];
 
+  void _selectedPage(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+/*
+  @override
+  void initState() {
+
+    _screens = [
+      {
+        'page': HomePage(),
+      },
+      {
+        'page': FeedPage(),
+      },
+      {
+        'page': SearchPage(),
+      },
+      {
+        'page': CartPage(),
+      },
+      {
+        'page': ProfilePage(),
+      }
+    ];
+
+    // TODO: implement initState
+    super.initState();
+  }
+*/
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
+        notchMargin: 2.0,
+        clipBehavior: Clip.antiAlias,
         shape: CircularNotchedRectangle(),
         child: BottomNavigationBar(
-            backgroundColor: Colors.white,
-            selectedItemColor: Colors.purple,
-            //onTap: _screens[currentIndex],
-            items: [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  title: Text(""),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.feed),
-                title: Text(""),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                title: Text(""),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart),
-                title: Text(""),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                title: Text(""),
-              ),
-            ],
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.purple,
+          unselectedItemColor: Colors.grey,
+          onTap: _selectedPage,
+          currentIndex: currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+              tooltip: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.rss_feed),
+              label: "Feed",
+              tooltip: "Feed",
+            ),
+            BottomNavigationBarItem(
+              activeIcon: null,
+              icon: Icon(null),
+              label: "",
+              tooltip: "Search",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag),
+              label: "Cart",
+              tooltip: "Cart",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+              tooltip: "Profile",
+            ),
+          ],
         ),
       ),
-      body: _screens[currentIndex],
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FloatingActionButton(
+          backgroundColor: Colors.purple,
+          tooltip: "Search",
+          child: Icon(Icons.search),
+          onPressed: () {
+            setState(() {
+              currentIndex = 2;
+            });
+          },
+        ),
+      ),
+     body: _screens[currentIndex],
     );
   }
 }
+
+// shortcut key comment single line ctrl + /
+// multiline comment ctrl + shift + /
