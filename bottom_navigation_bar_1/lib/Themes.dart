c087b5bc883791d87123f925377f4b2d282a0978
Bilal Ahmad <thinkingall78@gmail.com>
2021-06-19 13:30:45 +0500

    Change Theme Button
    Now button add into the app to change the theme.

--- bottom_navigation_bar_1/lib/Themes.dart
+++ bottom_navigation_bar_1/lib/Themes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  ThemeMode themeMode = ThemeMode.system;
+  ThemeMode themeMode = ThemeMode.light;
 
   bool get isDarkMode => themeMode == ThemeMode.dark;
 
--- bottom_navigation_bar_1/lib/screens/ProfileScreen.dart
+++ bottom_navigation_bar_1/lib/screens/ProfileScreen.dart
@@ -1,18 +1,41 @@
+import 'package:bottom_navigation_bar_1/Widgets/ChangeTheme.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        centerTitle: true,
+        title: Text("Profile Screen", style: TextStyle(fontSize: 18, color: Theme.of(context).textSelectionColor),),
+      ),
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Profile Screen", style: TextStyle(fontSize: 45, color: Theme.of(context).textSelectionColor, fontWeight: FontWeight.bold),),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 60,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Change Theme",
+                    style: TextStyle(fontSize: 20.0, color: Theme.of(context).textSelectionColor),
+                    ),
+                    ChangeThemeButtonWidget(),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
         ),
       ),
     );
